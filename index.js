68c94ade92f8ef07473903a94fcecf7f862712c7
Nicolas Gotchac <ngotchac@gmail.com>
2017-12-11 16:10:31 +0100

    Update config

--- index.js
+++ index.js
@@ -17,7 +17,70 @@
 
 'use strict';
 
-// Do not use React-Script own paths
-process.env.RS_SKIP_OWN = 1;
+const path = require('path');
+const { spawn } = require('child_process');
+const ora = require('ora');
+const fs = require('fs-extra');
 
-require('./react-scripts/bin/react-scripts');
+const DAPP_DIRECTORY = fs.realpathSync(process.cwd());
+
+// If linting, spawn a new process and pass all the arguments
+if (process.argv[2] === 'lint') {
+  lint().catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+} else {
+  // Do not use React-Script own paths
+  process.env.RS_SKIP_OWN = 1;
+
+  // Set the configuration path
+  process.env.CONFIG = path.resolve(__dirname, 'config.js');
+
+  // Run the main React Scripts
+  require('./react-scripts/bin/react-scripts');
+}
+
+async function aspawn (command, args) {
+  return new Promise((resolve) => {
+    spawn(command, args, {
+      cwd: DAPP_DIRECTORY,
+      stdio: 'inherit'
+    }).on('exit', (code) => {
+      if (code !== 0) {
+        process.exit(code);
+      }
+
+      resolve();
+    });
+  });
+}
+
+async function lint () {
+  const args = process.argv.length > 3
+    ? process.argv.slice(3)
+    : [ 'src' ];
+
+  const dappEslint = path.resolve(DAPP_DIRECTORY, './node_modules/.bin/eslint');
+  const selfEslint = path.resolve(__dirname, './node_modules/.bin/eslint');
+
+  const dappStylelint = path.resolve(DAPP_DIRECTORY, './node_modules/.bin/stylelint');
+  const selfStylelint = path.resolve(__dirname, './node_modules/.bin/stylelint');
+
+  const eslint = fs.existsSync(dappEslint)
+    ? dappEslint
+    : selfEslint;
+
+  const stylelint = fs.existsSync(dappStylelint)
+    ? dappStylelint
+    : selfStylelint;
+
+  const spinner = ora('Linting JS').start();
+
+  await aspawn(eslint, args);
+  spinner.succeed();
+
+  spinner.start('Linting CSS');
+  await aspawn(stylelint, [ './src/**/*.css' ]);
+  spinner.succeed();
+}
